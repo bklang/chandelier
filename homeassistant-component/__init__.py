fb63212f9cc8ede12fff74f0284372a593811710
Ben Klang <bklang@wirehack.net>
2022-07-03 20:47:38 -0400

    Some light refactoring

--- homeassistant-component/__init__.py
+++ homeassistant-component/__init__.py
@@ -1,5 +1,1 @@
 """Example Lights integration."""
-DOMAIN = 'chandelier'
-
-def setup(hass, config):
-    return True
--- homeassistant-component/light.py
+++ homeassistant-component/light.py
@@ -1,14 +1,19 @@
-"""Platform for light integration."""
+"""Platform for chandelier light integration."""
+from __future__ import annotations
+
 import logging
-import requests
 
 import voluptuous as vol
+import requests
 
-import homeassistant.helpers.config_validation as cv
 # Import the device class from the component that you want to support
-from homeassistant.components.light import (
-    ATTR_BRIGHTNESS, PLATFORM_SCHEMA, Light)
+import homeassistant.helpers.config_validation as cv
+from homeassistant.components.light import (ATTR_BRIGHTNESS, PLATFORM_SCHEMA,
+                                            LightEntity)
 #from homeassistant.const import CONF_HOST, CONF_PASSWORD, CONF_USERNAME
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -20,20 +25,27 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
 })
 
 
-def setup_platform(hass, config, add_entities, discovery_info=None):
-    """Set up the Awesome Light platform."""
+def setup_platform(
+    hass: HomeAssistant,
+    config: ConfigType,
+    add_entities: AddEntitiesCallback,
+    discovery_info: DiscoveryInfoType | None = None
+) -> None:
+    """Set up the Chandelier Light platform."""
+    _LOGGER.info("Setting up Chandelier")
 
     # Add devices
-    lights = [0,1,2,3,4,5,6,7,8,9]
-    add_entities(ChandelierBulb(light) for light in lights)
+    lights = ["chandelier"]
+    add_entities(Chandelier(light) for light in lights)
 
 
-class ChandelierBulb(Light):
-    """Representation of a bulb in the Chandelier."""
+class Chandelier(LightEntity):
+    """Representation of a Chandelier."""
 
     def __init__(self, idx):
-        """Initialize a ChandelierBulb."""
-        self._name = "Chandelier Bulb %d" % idx
+        """Initialize a Chandelier."""
+        _LOGGER.info("Adding chandelier %s" % idx)
+        self._name = idx
         self._idx = idx
         self._state = None
 
@@ -54,12 +66,12 @@ class ChandelierBulb(Light):
         brightness control.
         """
         self._state = True
-        requests.post("http://chandelier.local:4000/api/switch", data = {"light": self._idx, "direction": "on"})
+        requests.post("http://chandelier.local:4000/api/set", data = {"id": self._idx, "value": 1})
 
     def turn_off(self, **kwargs):
         """Instruct the light to turn off."""
         self._state = False
-        requests.post("http://chandelier.local:4000/api/switch", data = {"light": self._idx, "direction": "off"})
+        requests.post("http://chandelier.local:4000/api/set", data = {"id": self._idx, "value": 0})
 
     def update(self):
         """Fetch new state data for this light.
